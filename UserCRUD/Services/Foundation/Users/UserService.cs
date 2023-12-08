30cdacfa790aba4f089beb5cc866c8e08b133724
hicadok105@ridteam.com <hicadok105@ridteam.com>
2023-12-08 14:17:26 +0500

    INFRA: User Service Complete

---
+++ UserCRUD/Services/Foundation/Users/UserService.cs
@@ -0,0 +1,19 @@
+﻿using UserCRUD.Brokers.Storages;
+using UserCRUD.Models.Users;
+
+namespace UserCRUD.Services.Foundation.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly IStorageBroker strogaBroker;
+
+        public UserService(IStorageBroker strogaBroker)
+        {
+            this.strogaBroker = strogaBroker;
+        }
+
+        public User AddUSer(User user)=>
+            this.strogaBroker.InsertUser(user);   
+        
+    }
+}
