961cc28d386d0bbb0dc4e64cfb694d05ccf965ed
hicadok105@ridteam.com <hicadok105@ridteam.com>
2023-12-08 13:26:43 +0500

    CODE RUB:Change Model Name Model->User

---
+++ UserCRUD/Models/Users/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserCRUD.Models.Users
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
